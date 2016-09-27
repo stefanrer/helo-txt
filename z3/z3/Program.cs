8c4441b96ce0bddb2d90118544ab69f9a9351bbe
stefanrer <robstef85@gmail.com>
2016-09-27 22:32:17 +0300

    Add files via upload

---
+++ z3/z3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+class Operator
+{
+    public static void Main()
+    {
+        double x, y, g;
+        string str;       // Рабочая строка для ввода и вывода данных 
+        Console.Clear(); //Очищение строки
+        Console.Write("Введите X: ");
+        str = Console.ReadLine();
+        x = double.Parse(str);
+        Console.Write("Введите Y: ");
+        str = Console.ReadLine();
+        y = double.Parse(str);
+
+        if (x < y && x > 0)
+            g = x + Math.Sin(y);
+        else if (x > y && x < 0)
+            g = y - Math.Cos(x);
+        else g = 0.5 * x * y;
+        str = g.ToString();
+        Console.WriteLine("g=" + str);
+        Console.ReadLine();
+    } //Конец определения метода
+} //Конец объявления класса
+
