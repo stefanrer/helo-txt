f74f702db6037d36da97f325d8d3929ce221a825
stefanrer <robstef85@gmail.com>
2016-09-28 16:08:00 +0300

    Add files via upload

---
+++ Program2.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program {
+    static void Main(string[] args) {
+        Console.WriteLine("Введите число: ");
+        var x = int.Parse(Console.ReadLine());
+        //double x_4 = x % 4;
+        if ((x & 3) == 0)
+            Console.WriteLine(x / 4);
+        else
+            Console.WriteLine(x + 4);
+        //Console.ReadLine();
+    }
+}
