efd1d3aa16edaec6ab9fdb73e6030cf13cc14e49
stefanrer <robstef85@gmail.com>
2016-09-28 15:47:31 +0300

    Add files via upload

---
+++ Program1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число: ");
+            var x = double.Parse(Console.ReadLine());
+            double x_2 = x % 2;
+            if (x_2 == 0)
+                Console.WriteLine(x / 2);
+            else
+                Console.WriteLine(x = (x + 1) / 2);
+            Console.ReadLine();
+
+        }
+    }
+}
+
