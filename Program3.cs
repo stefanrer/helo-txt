14cd5e4cba0156f9cdae1c0667695fe498e720f4
stefanrer <robstef85@gmail.com>
2016-09-28 15:27:30 +0300

    Add files via upload

---
+++ Program3.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число от 1 до 10");
+             var x = double.Parse(Console.ReadLine());
+             if (x > 3) Console.WriteLine("Зачет");
+             else Console.WriteLine("Незачет");
+            Console.ReadLine();
+
+
+            
+
+            
+        }
+    }
+}
